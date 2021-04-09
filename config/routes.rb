dce63a158a261ea9622607380c48c9444d87232f
d-saori <60836449+d-saori@users.noreply.github.com>
2021-04-10 01:14:17 +0900

    ゲストユーザー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'graphs#index'
+  devise_for :users, controllers: {
+    registrantions: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # updateに「:id」は不要なのでresource「s」ではない事に注意
   resource :graphs, only: %i[index create update]
 end
