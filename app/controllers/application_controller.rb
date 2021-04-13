8f9096608b9fdc1e82b6ed26293ef7a95956a273
d-saori <60836449+d-saori@users.noreply.github.com>
2021-04-13 22:03:04 +0900

    アカウント編集ページview編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,14 @@ class ApplicationController < ActionController::Base
   # 全ページをログイン必須とする
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
     def configure_permitted_parameters
       added_attrs = [:email, :password, :username, :age, :gender, :height, :weight]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: [added_attrs, :profile, :target_weight, :target_body]
       devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
 end
