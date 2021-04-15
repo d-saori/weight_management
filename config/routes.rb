0676209f5b20961fc05af0a4f466f285b7bee051
d-saori <60836449+d-saori@users.noreply.github.com>
2021-04-15 18:36:39 +0900

    ゲストログインのルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   # updateに「:id」は不要なのでresource「s」ではない事に注意
-  resource :graphs, only: %i[index create update]
+  resource :graphs, only: %i[create update]
+  resources :graphs, only: [:index]
   resources :users, only: [:show]
 end
