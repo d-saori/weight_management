e3a0d8bcf2c3ecfe417ae11ba35c07301c132b0e
d-saori <60836449+d-saori@users.noreply.github.com>
2021-04-26 00:29:21 +0900

    マイページレイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,11 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 
-  get 'users/show'
   # get 'graph', to: 'graphs#search'
   root 'homes#top'
 
   # updateに「:id」は不要なのでresource「s」ではない事に注意
   resource :graphs, only: %i[create update]
   resources :graphs, only: [:index, :show]
-  # resources :users, only: [:show]
+  resources :users, only: [:show]
 end
