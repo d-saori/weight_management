9a0d4d81b2b69b45b48210f710f68ef6b4aeeedd
d-saori <60836449+d-saori@users.noreply.github.com>
2021-04-27 17:36:48 +0900

    userテスト用データ作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,14 @@
 FactoryBot.define do
   factory :user do
-    email { "satou@example.com" }
-    # password {}
-    username { "satou" }
-    age { 32 }
-    # height {}
-    # weight {}
-    # profile {}
-    # target_weight {}
-    # target_body {}
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 8) }
+    username { Faker::Name.name }
+    age { Faker::Number.within(range: 1..100) }
+    gender { Faker::Gender.binary_type }
+    height { Faker::Number.decimal(l_digits: 3, r_digits: 1) }
+    weight { Faker::Number.decimal(l_digits: 2, r_digits: 1) }
+    profile { Faker::Lorem.sentence }
+    target_weight { Faker::Number.decimal(l_digits: 3, r_digits: 1) }
+    target_body { Faker::Number.decimal(l_digits: 2, r_digits: 1) }
   end
 end
