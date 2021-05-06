d019f545c4ef2fdd9bf41312065db4788d49ee44
d-saori <60836449+d-saori@users.noreply.github.com>
2021-05-06 23:13:13 +0900

    新規登録時の入力欄変更, マイページのprofile項目の表示設定変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       added_attrs = [:email, :password, :password_confirmation]
-      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_up, keys: [added_attrs, :username]
       devise_parameter_sanitizer.permit :account_update, keys: [added_attrs, :username, :age, :gender, :height, :weight, :profile, :target_weight, :target_body, :avatar]
       devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
